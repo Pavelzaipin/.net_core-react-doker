ba1c2bfd7d857528e9f2e41392675d8327c3a03e
pasha.zai.1998@gmail.com <pasha.zai.1998@gmail.com>
2022-12-04 16:16:35 +0300

    create

---
+++ ExampleApplication/Controllers/GetCollectionController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleApplication.Controllers
+{
+    [Route("api/collection")]
+    [ApiController]
+    public class GetCollectionController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCollection()
+        {
+            var collection = new[]
+            {
+                new {Name = "One"},
+                new {Name = "Two"},
+                new {Name = "Three" }
+            };
+
+            return Ok(collection);
+        }
+    }
+}
---
+++ ExampleApplication/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleApplication.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = new[]
+            {
+                new { Name = "Pasha" },
+                new { Name = "Sasha" },
+                new { Name = "Dasha" },
+                new { Name = "Masha" },
+            };
+
+            return Ok(users);
+        }
+    }
+}
